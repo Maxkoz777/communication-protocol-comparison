60933148b270403ac51befba73234aa228d400b7
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-11-19 17:10:24 +0300

    java-rest-server added

---
+++ java-rest-server/src/test/java/com/example/javarestserver/JavaRestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javarestserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRestServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
