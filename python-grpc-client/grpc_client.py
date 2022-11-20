b294f5ca1b880ad7f9fe0cdc3156c40a3bad8a0f
Daniil Igudesman <digudesm@gmail.com>
2022-11-20 19:27:57 +0300

    grpc client updated

--- python-grpc-client/grpc_client.py
+++ python-grpc-client/grpc_client.py
@@ -5,7 +5,7 @@ import greeting_service_pb2
 
 
 def run():
-    with grpc.insecure_channel('localhost::9090') as channel:
+    with grpc.insecure_channel('127.0.0.1:9090') as channel:
         stub = greeting_service_pb2_grpc.GreetingServiceStub(channel)
         response = stub.greeting(greeting_service_pb2.HelloRequest(name='Daniil'))
     print(response)
